53a62c1f402fc8d90cd797861e883ebc764843cb
surendram123 <singhsurendram@gmail.com>
2018-05-31 08:52:26 +0530

    Cahnge done1

--- dev/components/about/AboutPage.js
+++ dev/components/about/AboutPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 class AboutPage extends React.Component {
     render() {
         return (<div>
-            About Page this     </div>
+            About Page </div>
         );
     }
 }
--- dev/index.js
+++ dev/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import App from './components/app';
 
 var HelloWorld = (name) => {
-    return "Hello sds surendra" + name;
+    return "Hello sds surendra12123" + name;
 }
 
 render(
